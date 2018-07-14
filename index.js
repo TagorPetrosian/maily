3f8b659a8ce0ebfd6a5c65eef810cb42d0fb81a6
Tagor Petrosian <p.tagor@gmail.com>
2018-07-14 20:40:34 +0300

    finished auth flow

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./server/config/keys");
+require("./server/models/user");
+require("./server/services/passport");
+
+mongoose.connect(
+  keys.mongoURI,
+  { useNewUrlParser: true }
+);
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "dude" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
 
-/* 
-  ######    A Few Steps Before Deploying To Heroku    ######
+app.use(passport.initialize());
+app.use(passport.session());
 
-  1. Dynamic Port Binding - Heroku tells us which port our app will use, so we need to make sure we listen to the port they tell us to
+require("./server/routes/authRoutes")(app);
 
-  2. Specify Node Environment - We want to use a specific version of node, so we need to tell heroku which version we want.
-  Added code below after main in the package.json file  
-  "engines": {
-    "node": "8.1.1",
-    "npm": "5.0.4"
-  }
-
-  3. Specify Start Script - Instruct Heroku what command to run to start our server running.
-  Added code below in "scripts" in the package.json file
-  "start": "node index.js"
-
-  4. Create .gitingnore - We don't want to include dependencies, Heroku will do that for us.
-  Added node_modules in the file
-*/
-
-// Dynamic Port Binding
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
