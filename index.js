4a30f52ba498f1a662858efd291f56f328a22fe8
Tagor Petrosian <p.tagor@gmail.com>
2018-07-15 16:39:17 +0300

    rearanged files

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
-const keys = require("./config/keys");
-require("./models/User");
-require("./services/passport");
+const keys = require("./server/config/keys");
+require("./server/models/User");
+require("./server/services/passport");
 
 mongoose.connect(
   keys.mongoURI,
