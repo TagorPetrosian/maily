3b43f94b0755e178bcb30810e3ab39d934ac61b3
Tagor Petrosian <p.tagor@gmail.com>
2018-07-15 16:48:38 +0300

    change authRoutes directory

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require("./routes/authRoutes")(app);
+require("./server/routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 
