bb0819cd437d1798abc03faf90b4a91fd9554f76
Tagor Petrosian <p.tagor@gmail.com>
2018-08-06 22:56:49 +0300

    Added dummy components to test routing

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Header extends Comment {
+  render() {
+    return (
+      <div>
+        <h2>Header</h2>
+      </div>
+    );
+  }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
+import reducers from "./reducers";
+// For starters we provide a dummy reducer that returns an array
+// The second arumegnt(initial state of app) is relevant when setting up server-side rendering
+const store = createStore(reducers, {}, applyMiddleware());
 
-ReactDom.render(<App />, document.querySelector("#root"));
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
