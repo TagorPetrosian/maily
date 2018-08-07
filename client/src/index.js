9220aecf424bfe81a70a5b1e1d06cd3cc552d487
Tagor Petrosian <p.tagor@gmail.com>
2018-08-07 14:23:44 +0300

    Added action creator fetchUser to verify login state

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,13 @@ import React from "react";
 import ReactDom from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
 // For starters we provide a dummy reducer that returns an array
 // The second arumegnt(initial state of app) is relevant when setting up server-side rendering
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDom.render(
   <Provider store={store}>
