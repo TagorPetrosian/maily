e99d9fafda69466e76e10c309913b7eb63a24413
Tagor Petrosian <p.tagor@gmail.com>
2018-08-08 00:31:43 +0300

    Implemented logout with redirect and added Landing comp.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+// import authReducer from "../reducers/authReducer";
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return "Still deciding";
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
     return (
       <div>
         <nav>
           <div className="nav-wrapper">
             <a className="left brand-logo">Maily</a>
-            <ul className="right">
-              <li>
-                <a>Login with Google</a>
-              </li>
-            </ul>
+            <ul className="right">{this.renderContent()}</ul>
           </div>
         </nav>
       </div>
@@ -19,4 +35,7 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+export default connect(mapStateToProps)(Header);
