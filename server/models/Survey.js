283bbcff075bd40991091d66413500d8d2a91d83
Tagor Petrosian <p.tagor@gmail.com>
2018-08-12 13:36:42 +0300

    Updated Survey model

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -10,7 +10,9 @@ const surveySchema = new Schema({
   recipients: [RecipientSchema],
   yes: { type: Number, default: 0 },
   no: { type: Number, default: 0 },
-  _user: { type: Schema.Types.ObjectId, ref: "User" } // _ indicates relationship field by convention
+  _user: { type: Schema.Types.ObjectId, ref: "User" }, // _ indicates relationship field by convention
+  dateSent: Date,
+  lastResponded: Date
 });
 
 mongoose.model("surveys", surveySchema);
